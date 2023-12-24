383d92f0317ae632b02048016015330f048d6592
vargasraquel <Raquel_Vargas@mckinsey.com>
2023-12-24 02:35:17 -0300

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true
-    }
-  }
+	plugins: [react()],
+	base: "/num-orixas-react/",
+	server: {
+		watch: {
+			usePolling: true,
+		},
+	},
 })
